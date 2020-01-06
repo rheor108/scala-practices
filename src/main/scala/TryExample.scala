421db6de1a87c0401e9aa6e742f2d753f9d10f01
rheor108 <developerbum@gmail.com>
2020-01-07 08:19:19 +0900

    try example

--- src/main/scala/TryExample.scala
+++ src/main/scala/TryExample.scala
@@ -1,3 +1,29 @@
+import scala.io.Source
+import java.net.URL
+import scala.util.Try
+
 object TryExample {
 
+  def parseURL(url: String): Try[URL] = Try(new URL(url))
+
+  def getURLContent1(url: String): Try[Iterator[String]] =
+    for {
+      url         <- parseURL(url)
+      connection  <- Try(url.openConnection())
+      is          <- Try(connection.getInputStream)
+      source      =  Source.fromInputStream(is)
+    } yield source.getLines()
+
+  // which is translated introduced
+  def getURLContent2(url: String): Try[Iterator[String]] =
+    parseURL(url).flatMap(
+      url => Try(url.openConnection()).flatMap(
+        connection => Try(connection.getInputStream).map(
+          is => {
+            val source = Source.fromInputStream(is)
+            source.getLines()
+          }
+        )
+      )
+    )
 }
